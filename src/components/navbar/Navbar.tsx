f2164b7ca5920aa23a0b0b851a91884b24825304
shailendr pawar <shailendrapawar980@gmail.com>
2026-02-17 00:08:47 +0530

    added: cart

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,18 +4,16 @@ import { navContainer, navItem } from "@/shared/animations/navbarVariants";
 import { Input } from "../ui/input";
 import { useNavigate } from "react-router-dom";
 
+import { TextAlignJustify } from 'lucide-react';
+import { useState } from "react";
+
 const Navbar = () => {
   const navigate = useNavigate();
+  const navToggle = useState(false);
 
   return (
     <main
-      className="
-sticky top-0 z-50
-    w-full h-24
-    flex justify-center items-center px-2
-bg-white/30 backdrop-blur-md
-shadow-[2px_5px_15px_rgba(255,255,255,0.25)]
-     "
+      className="sticky top-0 z-50 w-full h-24 flex justify-center items-center px-2 bg-white/30 backdrop-blur-md shadow-[2px_5px_15px_rgba(255,255,255,0.25)]"
     >
       <motion.section
         variants={navContainer}
@@ -35,7 +33,7 @@ shadow-[2px_5px_15px_rgba(255,255,255,0.25)]
         </motion.h3>
 
         {/* SEARCH BAR */}
-        <motion.div
+        {/* <motion.div
           variants={navItem}
           whileFocus={{ scale: 1.02 }}
           className="h-10 w-[45%] bg-gray-200 rounded-full"
@@ -46,21 +44,21 @@ shadow-[2px_5px_15px_rgba(255,255,255,0.25)]
             placeholder="Seach product"
             className=" h-full w-full text-sm sm:text-md outline-none border-none focus:outline-none focus:ring-0 focus-visible:ring-0"
           />
-        </motion.div>
+        </motion.div> */}
 
         {/* ACTION BUTTONS */}
-        <motion.div variants={navItem} className="flex gap-2 items-center">
+        <motion.div variants={navItem} className="flex gap-3 items-center">
           {/* WISHLIST */}
           <motion.button
             whileHover={{ scale: 1.08, y: -1 }}
             whileTap={{ scale: 0.9 }}
             transition={{ type: "spring", stiffness: 300, damping: 18 }}
-            className="h-9 w-9 sm:h-10 sm:w-10 flex items-center justify-center
+            className="size-7 md:size-8 flex items-center justify-center
                        rounded-full bg-gray-100 hover:bg-pink-400 hover:text-white
                        shadow-sm hover:shadow-md transition-all duration-300"
             onClick={() => navigate("/wishlist")}
           >
-            <Heart className="size-5 sm:size-6" />
+            <Heart className="size-4 sm:size-5" />
           </motion.button>
 
           {/* CART */}
@@ -68,13 +66,28 @@ shadow-[2px_5px_15px_rgba(255,255,255,0.25)]
             whileHover={{ scale: 1.08, y: -1 }}
             whileTap={{ scale: 0.9 }}
             transition={{ type: "spring", stiffness: 300, damping: 18 }}
-            className="h-9 w-9 sm:h-10 sm:w-10 flex items-center justify-center
+            className="size-7 md:size-8 flex items-center justify-center
                        rounded-full bg-gray-100 hover:bg-black hover:text-white
+                       shadow-sm hover:shadow-md transition-all duration-300
+                       mr-3"
+            onClick={() => navigate("/cart")}
+          >
+            <ShoppingBag className="size-4 sm:size-5" />
+          </motion.button>
+
+          {/* Profile */}
+          <motion.button
+            whileHover={{ scale: 1.08, y: -1 }}
+            whileTap={{ scale: 0.9 }}
+            transition={{ type: "spring", stiffness: 300, damping: 18 }}
+            className="size-9 sm:h-10 sm:w-10 flex items-center justify-center
+                       rounded-full bg-gray-300 hover:bg-gray-200 hover:text-white
                        shadow-sm hover:shadow-md transition-all duration-300"
             onClick={() => navigate("/cart")}
           >
-            <ShoppingBag className="size-5 sm:size-6" />
+
           </motion.button>
+
         </motion.div>
       </motion.section>
     </main>
--- src/features/cart/pages/CartPage.tsx
+++ src/features/cart/pages/CartPage.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 
 const CartPage = () => {
-  console.log("cart page loaded");
-  return <div>CartPage</div>;
+  // console.log("cart page loaded");
+  return <main className="h-auto py-5 px-2 w-full flex flex-col gap-5">
+    <h1 className="text-xl font-bold">Shopping Cart (1 items)</h1>
+
+    <section className="py-5 bg-amber-200">
+
+    </section>
+
+  </main>;
 };
 
 export default CartPage;
